e5faf956e0ef7a12f3cefe94e83ece64279c7311
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-11 18:10:31 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,98 @@
+const Discord = require("discord.js")
+const moment = require("moment")
+const prefix = "-H"
+const client = new Discord.Client();
+
+
+
+
+
+
+
+	client.on('message', message => {
+
+if (message.author.bot) return;
+  if (message.content.split(" ")[0].toLowerCase() === prefix + "قفل") {
+                        if(!message.channel.guild) return;
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You Need MANAGE_MESSAGES Permission').then(message => message.delete(5000))
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: false
+
+           }).then(() => {
+ const e = new Discord.RichEmbed()
+               .setAuthor('Channel Disabled By : '+message.author.username)
+                .setColor('#36393e')
+               
+               message.channel.send(e)
+               });
+             }
+if (message.content.split(" ")[0].toLowerCase() === prefix + "فتح") {
+    if(!message.channel.guild) return;
+
+if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You Need MANAGE_MESSAGES Permission').then(message => message.delete(5000))
+           message.channel.overwritePermissions(message.guild.id, {
+         SEND_MESSAGES: true
+
+           }).then(() => {
+               const e = new Discord.RichEmbed()
+               .setAuthor('Channel Enabled By : '+message.author.username)
+                        .setColor('#36393e')
+               
+               message.channel.send(e)
+           });
+             }
+
+
+
+});
+	
+	
+	
+	client.on('message',async message => {
+    if(message.content.startsWith(prefix + "setVoice")) {
+    if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('? **��� ���� ��������� �������**');
+    if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('? **��� ��� ��������� �������**');
+    message.channel.send('?| **�� ��� ����� �����**');
+    message.guild.createChannel(`Hano Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
+      console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
+      c.overwritePermissions(message.guild.id, {
+        CONNECT: false,
+        SPEAK: false
+      });
+      setInterval(function() {
+        c.setName(`Voice Online : [ ${message.guild.members.filter(m => m.voiceChannel).size} ]`)
+      },1000);
+    });
+    }
+  });
+  
+  
+  client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`System | Hano .`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+client.login(process.env.BOT_TOKEN);
