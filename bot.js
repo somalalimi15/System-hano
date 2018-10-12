e74a1870aa3606e62243fac39b2916a207d0d364
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-12 23:40:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,8 +67,50 @@ message.channel.send(id)
 
 client.on('guildMemberAdd', member=> {
 
-    client.channels.get("498032118547349514").send(`** :fire: | Welcome To __ Hano __ Server . ' ${member} :hearts:  .** | **آنـت عـضو رقـم **:**__ ${member.guild.memberCount}__** | ** By : ${Invite.inviter} **`);
+    client.channels.get("498032118547349514").send(`** :fire: | Welcome To __ Hano __ Server . ' ${member} :hearts:  .** | **آنـت عـضو رقـم **:**__ ${member.guild.memberCount}__** `);
     });
+
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "Hano Server")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', '😀║chatting・');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "Hano Server")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥ `)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
 
 client.on('message', async message => {
   let args = message.content.split(" ");
