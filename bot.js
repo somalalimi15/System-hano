6df5c7f68e3812099c1a92b9524c15c30b24cef5
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-13 19:49:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,6 +20,26 @@ const dateFormat = require('dateformat');
 
 const prefix = '$'
 
+
+  client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 5,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
+
+message.author.send(`**مدة الرابط : يـوم
+عدد استخدامات الرابط : 5 **`)
+
+
+    }
+});
+
 var ss = 0;
  
 client.on('voiceStateUpdate', (o,n) => {
